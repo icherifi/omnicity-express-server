a40b3ca6acb58ee8f194ae28ee3fe221016d9788
icherifi <icherifi.alaoui@gmail.com>
2024-09-01 12:38:52 +0400

    deployement

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-const PORT = 3000;
-
 app.use("/api", userRouter);
 
+const PORT = process.env.PORT || 3000; // Use the PORT provided by Render, default to 3000 if not available
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
